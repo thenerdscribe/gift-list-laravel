3cb890ab577c7097bc7b85bbc2b1252c626313a0
Ryan Morton <morton.ryan92@gmail.com>
2020-02-20 18:16:05 -0700

    HOTFIX: Add gift wasn't working

--- resources/views/gift/create.blade.php
+++ resources/views/gift/create.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <div class="container">
         @csrf
-        @if ( $gift )
+        @if ( isset($gift) )
             <gift-create method="patch" url="/gift/{{ $gift->id }}" gift="{{ $gift }}" ></gift-create>
         @else
-            <gift-create method="post" url="/gift/create/"></gift-create>
+            <gift-create method="post" url="/gift/create/" gift=""></gift-create>
         @endif
     </div>
 @endsection
